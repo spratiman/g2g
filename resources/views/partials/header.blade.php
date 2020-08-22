24ed23b102d65a91dd4cc8ac2d74d5023c85d97e
Pratiman Shahi <spratiman@yahoo.com>
2020-08-22 18:17:25 -0400

    Add mobile view responsiveness

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,12 +1,14 @@
 <header class="site-navbar py-2 js-sticky-header site-navbar-target" role="banner">
 
   <div class="container">
+      
+      
     <div class="row align-items-center">
       
-      <div class="col-4 col-md-4 col-xl-4  d-block">
+      <div class="col-6 col-md-4 col-xl-4  d-block">
         <a href="{{ url('/') }}" class="text-white h2 mb-0">
           <h2 class="mb-0 mt-1 site-logo white">{{config('app.name')}}</h2>
-          <h4 class="mb-0 ">{{config('app.desc')}}</h4>
+          <h4 id="content-desktop" class="mb-0 ">{{config('app.desc')}}</h4>
         </a>
       </div>
 
@@ -15,25 +17,25 @@
 
           <ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block ml-0 pl-0">
            <li>
-              <a href="{{ route('questioniar') }}" class="nav-link white">
+              <a href="{{ route('questioniar') }}" class="white">
                 <b><i class="fa fa-calculator" style="color:#fff"></i> Free Recommendation</b>
               </a>
             </li>
             <li>
-              <a href="#" class="nav-link white">
-                <b><i class="fa fa-question-circle" style="color:#fff"> Why {{config('app.name')}} </i> </b>
+              <a href="#" class="nav-link">
+                <b><i class="fa fa-question-circle" style="color:#fff"></i> Why {{config('app.name')}}  </b>
               </a>
             </li>
             <li>
             @guest
-              <a href="{{ route('login') }}" class="nav-link white"><b><i class="fa fa-user" style="color:#fff"></i> Login</b></a>
+              <a href="{{ route('login') }}" class="white"><b><i class="fa fa-user" style="color:#fff"></i> Login</b></a>
               @else
 
-              <a href="{{ route('home') }}" class="nav-link white"><b><i class="fa fa-home" style="color:#fff"></i> Dashboard</b></a>
+              <a href="{{ route('home') }}" class="white"><b><i class="fa fa-tachometer" style="color:#fff"></i> Dashboard</b></a>
               <a class="nav-link white" href="{{ route('logout') }}" onclick="event.preventDefault();
                   document.getElementById('logout-form').submit();">
                     
-                  <b><i class="fa fa-home" style="color:#fff"></i> {{ __('Logout') }}</b>
+                  <b><i class="fa fa-sign-out" style="color:#fff"></i> {{ __('Logout') }}</b>
               </a>
               <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                   @csrf
@@ -46,7 +48,7 @@
       </div>
 
 
-      <div class="col-6 col-md-9 d-inline-block d-lg-none ml-md-0" ><a href="#" class="site-menu-toggle js-menu-toggle text-black float-right"><span class="icon-menu h3"></span></a></div>
+      <div class="col-6 col-md-9 d-inline-block d-lg-none ml-md-0" ><a href="#" class="site-menu-toggle js-menu-toggle text-black float-right"><span class="icon-menu h3" style="color:white !important;"></span></a></div>
 
     </div>
   </div>
